6021bc1fa09352ad837b56a6ca27a7ea99afb9c8
Swetha <swethasenthilkmr2@gmail.com>
2026-05-13 18:30:56 +0530

    updated services section

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -105,7 +105,6 @@ export default function ServicesSection() {
                     </ul>
                   </div>
                 </div>
-
               </div>
             </div>
           ))}
